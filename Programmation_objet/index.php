8c97eb27f709bf9d9c32e28e90f2ae306cae7fa8
axce89 <62295841+axce89@users.noreply.github.com>
2020-03-18 09:05:34 +0100

    Add files via upload

--- Programmation_objet/index.php
+++ Programmation_objet/index.php
@@ -1,20 +1,17 @@
 <?php
-include "fonction.php";
 
 
+require "map.php";
 
+$r = new Room(Room::TREASURE_MONSTER_ROOM, 0, 0);
 
+$r->setType(Room::ROOM | Room::MONSTER)->setX(1)->setY(5);
 
-// $tabhorizontal = array("X", "X", "X", "X", "X");
-// $tabVertical = array();
-
-// for ($i = 0; $i < 5; $i ++) {
-//     $i + 1
-//     $i+1[$i] =  $tabhorizontal;
-// }
-
-// print_r (depart($tabhorizontal, $tabVertical));
-
-// affichageDamier($tabVertical);
+//Permet de lancer les fonctions affichage et générations de ROOM------------------
+//Nombre de room type "WALL" vertical et horisontal (donc 10 = 100 WALL)
+$map = new Map(10);
+//Génére un nombre de ROOM (premier chiffre, donc 10 = 10 ROOM dans 100 WALL)------
+$map->generation(10,1);
+$map->affichage();
 
 ?>
---
+++ Programmation_objet/map.php
@@ -0,0 +1,221 @@
+<?php
+
+require "room.php";
+
+class Map{
+    protected $_mapping;
+    private $_size;
+    
+    //Fonction pour créer un tableau représentant la carte (pas encore généré et affiché)-----------
+    public function __construct($size=5){
+        $this->_size = $size;
+        $this->mappings = array();
+        for($i=0; $i< $size; $i++){
+            $this->mappings[] = array();
+            for($j=0; $j < $size; $j++){
+                $this->mappings[$i][$j] = new Room(Room::WALL, $i, $j);
+            }
+        }
+    }
+
+    //Fonction pour afficher la carte sur le terminal avec les symboles des ROOM -----------------
+    public function affichage(){
+        $i=0;
+        foreach($this->mappings as $hori){
+            $y=0;
+            foreach($hori as $verti){
+                //Définie le symbole sur le "TERMINAL" ----------------------------------
+                if($this->mappings[$i][$y]->_type==0){
+                    echo '   ';
+                }elseif($this->mappings[$i][$y]->_type == Room::START){
+                    echo '[✪]';
+                }elseif($this->mappings[$i][$y]->_type == Room::ROOM){
+                    echo '[ ]';
+                }elseif($this->mappings[$i][$y]->_type == Room::BOSS){
+                    echo '[☠]';
+                }elseif ($this->mappings[$i][$y]->_type == Room::ROOM_TREASURE){
+                    echo '[€]';
+                }elseif ($this->mappings[$i][$y]->_type == Room::ROOM_MONSTER) {
+                    echo '[♀]';
+                }elseif ($this->mappings[$i][$y]->_type == Room::TREASURE_MONSTER_ROOM) {
+                    echo '[⁂]';
+                }elseif ($this->mappings[$i][$y]->_type == Room::ROOM_FIRE_CAMP) {
+                    echo '[♨]';
+                }
+                $y++;
+            }
+            echo PHP_EOL;
+            $i++;
+        }
+    }
+
+    //Cette fonction permet de créer une carte aléatoirement avec un nombre aléatoire de ROOM ----------------------------
+    public function generation($nbRoom=15,$nbBoss=1,$nbtreasure=2,$nbMonstre=5,$nbFeuDeCamp=1){
+        $h = mt_rand(1,$this->_size-2);
+        $v = mt_rand(1, $this->_size-2);
+        $this->mappings[$h][$v]->_type=Room::START;
+        $compteur = mt_rand(round((mt_rand(60,80)/100*$nbRoom), 0),$nbRoom);
+        /* echo $compteur; */
+        //Génération de ROOM aléatoire -----------------------------------------
+        $room = 0;
+        while($room <= $compteur){
+            $hr = mt_rand(0, $this->_size - 1);
+            $vr = mt_rand(0, $this->_size - 1);
+            $hasard = mt_rand(0, 10);
+            if(($hr > 0 && $hr < $this->_size-1) && ($vr > 0 && $vr < $this->_size - 1 && $this->mappings[$hr][$vr]->_type == Room::WALL)){
+                    if ($this->mappings[$hr + 1][$vr]->_type != 0 || $this->mappings[$hr-1][$vr]->_type != 0 || $this->mappings[$hr][$vr + 1]->_type != 0 || $this->mappings[$hr][$vr- 1]->_type != 0) {
+                        //condition pour générer des couloirs plus que des patées -------------------------
+                        if($hasard <= 9 && $this->mappings[$hr + 1][$vr]->_type != 0 && $this->mappings[$hr][$vr + 1]->_type == 0 && $this->mappings[$hr - 1][$vr]->_type == 0 && $this->mappings[$hr][$vr - 1]->_type == 0){
+                            $this->mappings[$hr][$vr]->_type = Room::ROOM;
+                            $room++;
+                        }
+                        if ($hasard <= 9 && $this->mappings[$hr + 1][$vr]->_type == 0 && $this->mappings[$hr][$vr + 1]->_type != 0 && $this->mappings[$hr - 1][$vr]->_type == 0 && $this->mappings[$hr][$vr - 1]->_type == 0) {
+                            $this->mappings[$hr][$vr]->_type = Room::ROOM;
+                            $room++;
+                        }
+                        if ($hasard <= 9 && $this->mappings[$hr + 1][$vr]->_type == 0 && $this->mappings[$hr][$vr + 1]->_type == 0 && $this->mappings[$hr - 1][$vr]->_type != 0 && $this->mappings[$hr][$vr - 1]->_type == 0) {
+                            $this->mappings[$hr][$vr]->_type = Room::ROOM;
+                            $room++;
+                        }
+                        if ($hasard <= 9 && $this->mappings[$hr + 1][$vr]->_type == 0 && $this->mappings[$hr][$vr + 1]->_type == 0 && $this->mappings[$hr - 1][$vr]->_type == 0 && $this->mappings[$hr][$vr - 1]->_type != 0) {
+                            $this->mappings[$hr][$vr]->_type = Room::ROOM;
+                            $room++;
+                        }
+                        if($hasard == 10){
+                            //garder les 2 lignes du dessous si on doit remettre à zero
+                            $this->mappings[$hr][$vr]->_type=Room::ROOM;
+                            $room++;
+                        }
+                    }
+            }
+
+        }
+        //Générer un boss en position aléatoire mais toujours au fond d'un couloir----------------------------
+        $boss = 0;
+        while($boss < $nbBoss){
+            $hr = mt_rand(0, $this->_size - 1);
+            $vr = mt_rand(0, $this->_size - 1);
+            if (($hr > 0 && $hr < $this->_size - 1) && ($vr > 0 && $vr < $this->_size - 1 && $this->mappings[$hr][$vr]->_type == Room::WALL)) {
+                if ($this->mappings[$hr + 1][$vr]->_type != 0 || $this->mappings[$hr - 1][$vr]->_type != 0 || $this->mappings[$hr][$vr + 1]->_type != 0 || $this->mappings[$hr][$vr - 1]->_type != 0) {
+                    if ($this->mappings[$hr + 1][$vr]->_type != 0 && $this->mappings[$hr][$vr + 1]->_type == 0 && $this->mappings[$hr - 1][$vr]->_type == 0 && $this->mappings[$hr][$vr - 1]->_type == 0) {
+                        $this->mappings[$hr][$vr]->_type = Room::BOSS;
+                        $boss++;
+                    }
+                    if ($this->mappings[$hr + 1][$vr]->_type == 0 && $this->mappings[$hr][$vr + 1]->_type != 0 && $this->mappings[$hr - 1][$vr]->_type == 0 && $this->mappings[$hr][$vr - 1]->_type == 0) {
+                        $this->mappings[$hr][$vr]->_type = Room::BOSS;
+                        $boss++;
+                    }
+                    if ($this->mappings[$hr + 1][$vr]->_type == 0 && $this->mappings[$hr][$vr + 1]->_type == 0 && $this->mappings[$hr - 1][$vr]->_type != 0 && $this->mappings[$hr][$vr - 1]->_type == 0) {
+                        $this->mappings[$hr][$vr]->_type = Room::BOSS;
+                        $boss++;
+                    }
+                    if ($this->mappings[$hr + 1][$vr]->_type == 0 && $this->mappings[$hr][$vr + 1]->_type == 0 && $this->mappings[$hr - 1][$vr]->_type == 0 && $this->mappings[$hr][$vr - 1]->_type != 0) {
+                        $this->mappings[$hr][$vr]->_type = Room::BOSS;
+                        $boss++;
+                    }
+
+
+
+                }
+            }
+        }
+        // Générer des trésors en position aléatoirs mais dans une ROOM ------------------------
+        $treasure = 0;
+            // Générer automatiquement un trésor en % de la taille de la MAP------------------------
+        $hasardTreasure = mt_rand(10,15);
+        $nbtreasure = round($nbRoom / 100 * $hasardTreasure);
+
+        while($treasure < $nbtreasure){
+            $hr = mt_rand(0, $this->_size - 1);
+            $vr = mt_rand(0, $this->_size - 1);
+            if ($this->mappings[$hr][$vr]->_type == 1 ) {
+                $this->mappings[$hr][$vr]->_type = Room::ROOM | Room::TREASURE;
+                $treasure++;
+            }
+        }
+        // Générer des monstres en position aléatoirs mais dans une ROOM ------------------------
+        $monstre = 0;
+            // Générer automatiquement des monstres en % de la taille de la MAP------------------------
+        $hasardMonstre = mt_rand(20, 30);
+        $nbMonstre = round($nbRoom / 100 * $hasardMonstre);
+
+        while($monstre < $nbMonstre){
+            $hr = mt_rand(0, $this->_size - 1);
+            $vr = mt_rand(0, $this->_size - 1);
+            $hasardMonstreTreasure = mt_rand(1, 10);
+            if ($this->mappings[$hr][$vr]->_type == 1) {
+                if($hasardMonstreTreasure <= 8){
+                    $this->mappings[$hr][$vr]->_type = Room::MONSTER | Room::ROOM;
+                    $monstre++;
+                }
+                if ($hasardMonstreTreasure >= 9) {
+                    $this->mappings[$hr][$vr]->_type = Room::ROOM | Room::TREASURE | Room::MONSTER;
+                    $monstre++;
+                }
+            }
+        }
+        // Générer un feu de camp en position aléatoirs mais dans une ROOM ------------------------
+        $feuDeCamp = 0;
+            // Générer automatiquement un feu de camp en % de la taille de la MAP (20 room = 2 feux)------------------------
+        $nbFeuDeCamp = round($nbRoom / 100 * 5);
+        while ($feuDeCamp < $nbFeuDeCamp) {
+            $hr = mt_rand(0, $this->_size - 1);
+            $vr = mt_rand(0, $this->_size - 1);
+            if ($this->mappings[$hr][$vr]->_type == 1) {
+                $this->mappings[$hr][$vr]->_type = Room::ROOM | Room::FIRE_CAMP;
+                $feuDeCamp++; 
+            }
+        }
+    }
+}
+
+
+
+                    // ______§§§§§____________________________________________________________§§§§§§§§_________
+                    // _______§§§§§§§§§______________________________________________________§§§§§§§§§§_________
+                    // ________§§§§§§§§§§§§________________________________________________§§§§§§§§§§§§_________
+                    // ________§§§§§§§§§§§§§§§§__________________________________________§§§§§§§§§§§§§§_________
+                    // _________§§§§§§§§§§§§§§§§§§_____________________________________§§§§§§§§§§§§§§§§_________
+                    // _________§§§§§§§§§§§§§§§§§§§§§_________________________________§§§§§§§§§§§§§§§§§_________
+                    // __________§§§§§§§§§§§§§§§§§§§§§§§_____________________________§§§§§§§§§§§§§§§§§__________
+                    // ___________§§§§§§§§§§§§§§§§§§§§§§§§__________________________§§§§§§§§§§§§§§§§§§__________
+                    // _____________§§§§§§§§§§§§§§§§§§§§§§§§§______________________§§§§§§§§§§§§§§§§§§§__________
+                    // ______________§§§§§§§§§§§§§§§§§§§§§§§§§§___________________§§§§§§§§§§§§§§§§§§§§__________
+                    // ________________§§§§§§§§§§§§§§§§§§§§§§§§§_________________§§§§§§§§§§§§§§§§§§§§___________
+                    // _________________§§§§§§§§§§§§§§§§§§§§§§§§§§_______________§§§§§§§§§§§§§§§§§§§§___________
+                    // ___________________§§§§§§§§§§§§§§§§§§§§§§§§§§_____________§§§§§§§§§§§§§§§§§§§____________
+                    // ______________________§§§§§§§§§§§§§§§§§§§§§§§§§__________§§§§§§§§§§§§§§§§§§§§____________
+                    // ________________________§§§§§§§§§§§§§§§§§§§§§§§§§________§§§§§§§§§§§§§§§§§§§_____________
+                    // ___________________________§§§§§§§§§§§§§§§§§§§§§§§_______§§§§§§§§§§§§§§§§§§______________
+                    // ______________________________§§§§§§§§§§§§§§§§§§§§§§_____§§§§§§§§§§§§§§§§§_______________
+                    // ___________________________________§§§§§§§§§§§§§§§§§§§___§§§§§§§§§§§§§§§§________________
+                    // ________________________________________§§§§§§§§§§§§§§§__§§§§§§§§§§§§§§__________________
+                    // ______________________________________________§§§§§§§§§§§§§§§§§§§§§§§§___________________
+                    // ________________________________________§§§§§§§§§§§§§§§§§§§§§§§§§§§§_____________________
+                    // _________________________________§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§_______________________
+                    // ______________________________§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§______________________
+                    // ___________________________§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§____________________
+                    // _________________________§§§§§§§§_____§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§___________________
+                    // _______________________§§§§§§§§§_______§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§__________________
+                    // _____________________§§§§§§§§§§§_______§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§________________
+                    // ____________________§§§§§§§§§§§§§_____§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§________________
+                    // ___________________§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§________________
+                    // ___________________§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§MaBite§§§§§§§§§§§§________________
+                    // ___________________§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§________________
+                    // ___________________§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§________________
+                    // ____________________§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§________________
+                    // ______________________§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§________________
+                    // _________________________§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§_________________
+                    // _____________________________§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§__________________
+                    // ___________________________________§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§__________________
+                    // _____________________________________§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§___________________
+                    // _____________________________________§§§§§§§§§§§§§§§§§§§§§§§§§§§§§_______________________
+                    // ____________________________________§§§§§§§§§§_____§§§§§§§§§§____________________________
+                    // _____________________________________§§§§§_________§§§§§_________§§§§§___________________
+                    // ______________________________§§§§_________§§§§____________§§§§§§§§§_____________________
+                    // ______________________________§§§§§§§§§§§§§§§§§_____§§§§§§§§§§___________________________
+                    // ______________________________§§§§§§§§§§§§§§§§§___§§§§§__________________________________
+                    // ______________________________§§§§§______§§§§§§__________________________________________
+                    // ____________________________________________§§§__________________________________________
+                    // _________________________________________________________________________________________
+?>
